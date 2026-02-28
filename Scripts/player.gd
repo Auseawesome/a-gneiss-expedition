17cebb2932327759e11984852eb347e44b2a32b3
Virtuearles <aeearles@icloud.com>
2026-02-28 14:27:57 +0000

    player stats and follow cursor

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 @export var speed = 400
 var is_moving
+var max_health=50
+var current_health
+var strength=10
 
 func _ready() -> void:
 	GlobalData.player = self
+	current_health=max_health
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
+	var mouse_pos = get_viewport().get_mouse_position()
+	mouse_pos += global_position
+	mouse_pos -= Vector2(get_viewport().get_window().size) / 2
+	look_at(mouse_pos)
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
