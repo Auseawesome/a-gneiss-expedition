58f16568d64dc85c9f97a83de0ce86f93b9e3d09
Virtuearles <aeearles@icloud.com>
2026-02-28 17:40:58 +0000

    shop built

---
+++ Scripts/sheild.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		do_action()
+
+func do_action():
+	GlobalData.coins-=100
+	queue_free()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Scripts/speed_potion.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		do_action()
+
+func do_action():
+	GlobalData.coins-=100
+	queue_free()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
