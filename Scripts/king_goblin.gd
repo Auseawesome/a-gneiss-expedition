dbbade94d8e67f01fe74df9e5a35bdf395157545
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-28 13:56:13 +0000

    Kind Goblin Waves

--- Scripts/king_goblin.gd
+++ Scripts/king_goblin.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 enum State { PATROL, CHASE, ATTACK }
 var state = State.PATROL
-
+var health = 30
+var first_wave_done: bool = false;
+var second_wave_done: bool = false;
+var third_wave_done: bool = false;
+var goblin_link = preload("res://Scenes/Goblin.tscn")
 const MAX_SPEED = 200
-
 var acceleration = 8
-
 @onready var raycast = $RayCast2D
 
 func _ready():
@@ -23,11 +25,16 @@ func _physics_process(delta):
 	velocity = clamp(velocity.length(), 0, MAX_SPEED) * velocity.normalized()
 	
 	look_at(GlobalData.player.global_position)
-	
+	if(health < 40 && first_wave_done != true):
+		for i in range(2):
+			var instance = goblin_link.instantiate()
+			get_parent().add_child(instance)
+			first_wave_done = true;
 	# Check for collision and if it would collide, accelerate away
 	var collision = move_and_collide(velocity * delta, true)
 	if(collision):
 		velocity += collision.get_normal() * acceleration * 100
 	
+
 	# Move goblin
 	move_and_collide(velocity * delta)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 @export var speed = 400
 var is_moving
+
+func _ready() -> void:
+	GlobalData.player = self
+
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
