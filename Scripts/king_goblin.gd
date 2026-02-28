571772c17ba2d7498162cfbad411d967bc861e79
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-28 15:06:01 +0000

    Goblin waves done

--- Scripts/king_goblin.gd
+++ Scripts/king_goblin.gd
@@ -6,21 +6,21 @@ var goblin_scene = preload("res://Scenes/Goblin.tscn")
 func _ready() -> void:
 	super._ready()
 	max_health = 150
-	current_health = max_health
+	current_health = 30
 func _physics_process(delta: float) -> void:
 	goblin_movement(delta)
 	if(current_health <= 125 && current_wave == 0):
 		for i in range(2):
 			var instance = goblin_scene.instantiate()
 			get_parent().add_child(instance)
-			current_wave == 1;
+			current_wave += 1;
 	if(current_health <= 100 && current_wave == 1):
 		for i in range(2):
 			var instance = goblin_scene.instantiate()
 			get_parent().add_child(instance)
-			current_wave == 2;
+			current_wave += 1;
 	if(current_health <= 50 && current_wave == 2):
 		for i in range(2):
 			var instance = goblin_scene.instantiate()
 			get_parent().add_child(instance)
-			current_wave == 3;
+			current_wave += 3;
