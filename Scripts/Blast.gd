89322619928a0c6428890ee0fe0bb4f638cfbbcc
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-02-28 18:05:54 +0000

    Sorcerer blast hits player

--- Scripts/Blast.gd
+++ Scripts/Blast.gd
@@ -1,12 +1,13 @@
 extends Area2D
-
 @export var speed: float = 400.0
 var direction: Vector2 = Vector2.ZERO
-
+var damage = 10
 func _process(delta: float) -> void:
-	# Move the bullet in the assigned direction
 	position += direction * speed * delta
 
-func _on_body_entered(body: Node2D) -> void:
-	if not body.is_in_group("Enemies"): 
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body is Player:
+		GlobalData.player.current_health -= damage
+		queue_free()
+	elif body is StaticBody2D or body is TileMap:
 		queue_free()
